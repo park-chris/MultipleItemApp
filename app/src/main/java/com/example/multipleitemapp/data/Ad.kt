002e3ded4ef159caa7c342c0a5ffa2fe7bdf8580
박수정 <zxzx4342@naver.com>
2024-02-15 03:01:01 +0900

    1-3 각 데이터에 맞는 레이아웃 생성

--- app/src/main/java/com/example/multipleitemapp/data/Ad.kt
+++ app/src/main/java/com/example/multipleitemapp/data/Ad.kt
@@ -1,7 +1,7 @@
 package com.example.multipleitemapp.data
 
 data class Ad(
-    val it: Int = -1,
+    val title: String = "",
     val imgUrl: String = ""
 ): Item {
     override val viewType: ViewType
